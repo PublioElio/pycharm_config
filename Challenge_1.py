93f79ccb771a290d2c8ba9c31308c82b5cf2e73d
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2024-10-30 22:51:44 +0100

    Update Challenge_1.py

--- Challenge_1.py
+++ Challenge_1.py
@@ -3,3 +3,11 @@ def sum_num(a, b, c):
     if a == b == c:
         total *= 2
     return total
+
+
+def sqrt(num):
+    estimation = num / 2.0
+    tolerance = 0.00001
+    while abs(estimation * estimation - num) > tolerance:
+        estimation = (estimation + num / estimation) / 2.0
+    return round(estimation, 3)
