3ae749a8e91d0e3d9a21a5096c39237c7129f33b
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2024-10-31 07:35:32 +0100

    Update Challenge_1.py

--- Challenge_1.py
+++ Challenge_1.py
@@ -1,4 +1,4 @@
-# challenge 1
+# challenge 01
 def sum_num(a, b, c):
     total = (a + b + c)
     if a == b == c:
@@ -6,7 +6,7 @@ def sum_num(a, b, c):
     return total
 
 
-# challenge 2
+# challenge 02
 def sqrt(num):
     estimation = num / 2.0
     while abs(estimation * estimation - num) > 0.00001:
@@ -14,11 +14,14 @@ def sqrt(num):
     return round(estimation, 3)
 
 
-# challenge 3
+# challenge 03
 def quot_rem(num1, num2):
     return round(num1 / num2, 2), num1 % num2
 
 
-# challenge 4
+# challenge 04
 def even_odd(num):
     return "Even" if num % 2 == 0 else "Odd"
+
+
+# challenge 05
